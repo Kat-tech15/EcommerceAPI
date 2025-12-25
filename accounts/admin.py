7f80dd745e2dae9363129343b7240c96094d58b7
Kat-tech15 <kelvinkatwai@gmail.com>
2025-12-25 20:32:35 +0300

    modified orders/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import EmailOTP
+from .models import EmailOTP, ContactMessage, Profile
 
 admin.site.register(EmailOTP)
+admin.site.register(Profile)
+admin.site.register(ContactMessage)
+
--- reviews/serializers.py
+++ reviews/serializers.py
@@ -3,10 +3,7 @@ from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
     reviewer = serializers.StringRelatedField(read_only=True)
-<<<<<<< HEAD
-=======
     rating = serializers.IntegerField(min_value=1, max_value=5)
->>>>>>> f64f5bee031111815c913d53012d6891b85dbca9
     
     class Meta:
         model = Review
